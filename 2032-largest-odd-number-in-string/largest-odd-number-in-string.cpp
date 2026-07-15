909b81c38832d947232a461c5d0ce87060859337
Katuru Bharath <bharathyadav.katuru07@gmail.com>
2026-07-15 11:35:05 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (99.66%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution{	
+public:		
+    string largestOddNumber(string& s){
+        int n=s.size();
+        for(int i=n-1;i>=0;i--){
+            if((s[i]-'0')%2==1){
+                return s.substr(0,i+1);
+            }
+        }
+        return "";
+    }
+
+};
