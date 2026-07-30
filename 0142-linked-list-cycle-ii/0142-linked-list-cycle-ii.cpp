914c2d4c37ef6b939ab50f2511fd39a581acaf99
Katuru Bharath <bharathyadav.katuru07@gmail.com>
2026-07-30 20:58:56 +0530

    Time: 3 ms (98.1%), Space: 11.3 MB (86.67%) - LeetHub - Time: O(n), Space: O(1)

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -16,7 +16,7 @@ public:
             fast=fast->next->next;
             if(slow==fast){
                 slow=head;
-                while(slow!=fast){
+                while(slow != fast){
                     slow=slow->next;
                     fast=fast->next;
                 }
