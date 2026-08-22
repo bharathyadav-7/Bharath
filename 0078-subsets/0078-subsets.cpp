9cc71b841ee660eef8b956624a7b2b2c861cd522
Katuru Bharath <bharathyadav.katuru07@gmail.com>
2026-08-22 18:09:49 +0530

    Time: 2 ms (36.65%), Space: 10.1 MB (37.64%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        for(int i=0;i<(1<<n);i++){
+            vector<int> sub;
+            for(int j=0;j<n;j++){
+                if(i & (1<<j)){
+                    sub.push_back(nums[j]);
+                }
+            }
+            ans.push_back(sub);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
